d5a9413f8a96919d0fca50c3b3bf357a3dea93d3
ergusto <fergus.ruston@kalorgroup.com>
2023-03-19 10:02:55 +0000

    Split sign up and sign in into two separate forms

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -8,17 +8,17 @@ export default function LogIn() {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col text-center space-y-2">
             <h1 className='fixed top-0 left-0 right-0 pt-4 mx-auto font-semibold tracking-tight text-center text-1xl w-100'>Scores On The Doors</h1>
-            <h2 className="text-2xl font-semibold tracking-tight">Welcome back</h2>
-            <p className="text-sm text-slate-500">Enter your email address to sign in</p>
+            <h2 className="text-2xl font-semibold tracking-tight">Welcome</h2>
+            <p className="text-sm text-slate-500">Enter your email address to create an account</p>
           </div>
           <div className="grid gap-6">
-            <AuthorisationForm buttonText="Sign In" />
+            <AuthorisationForm buttonText="Sign Up" />
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <span className="w-full border-t border-slate-300"></span>
               </div>
               <div className="relative flex justify-center text-xs uppercase">
-                <span className="px-2 bg-white text-slate-600">Or <Link href="/auth/sign-up" className='underline decoration-black underline-offset-2 text-slate-600'>Sign Up</Link> </span>
+                <span className="px-2 bg-white text-slate-600">Or <Link href="/auth/sign-in" className='underline decoration-black underline-offset-2 text-slate-600'>Sign In</Link></span>
               </div>
             </div>
           </div>
---
+++ src/ui/components/authForm/index.tsx
@@ -0,0 +1,40 @@
+import { useRouter } from "next/router";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { signIn, useSession } from "next-auth/react";
+import { Button, Input } from "@/ui/primitives";
+
+interface AuthorisationFormProps {
+  buttonText: string;
+}
+
+type FormValues = {
+  email: string;
+};
+
+export default function AuthorisationForm({ buttonText = 'Sign In' }: AuthorisationFormProps) {
+  const router = useRouter();
+  const { status: sessionStatus } = useSession();
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
+
+  if (sessionStatus === "authenticated") {
+    router.push("/");
+  }
+
+  const onLoginFormSubmitted: SubmitHandler<FormValues> = async (credentials: FormValues) => {
+    signIn("email", { email: credentials.email });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onLoginFormSubmitted)}>
+      <div className="grid gap-2">
+        <div className="grid gap-1">
+          <label htmlFor="email" className="text-sm font-medium leading-none sr-only peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Email</label>
+          <Input {...register('email', { required: true })} className="peer" type="email" placeholder="Email" autoComplete="none" autoCapitalize="none" autoCorrect="off" name="email" />
+          {errors.email?.type === 'required' && <span className="px-1 text-xs text-red-600">Email is required</span>}
+        </div>
+        <Button>{buttonText}</Button>
+      </div>
+    </form>
+  );
+}
