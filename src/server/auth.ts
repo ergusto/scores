1063e0acf685a77a156aba824f19c03f6d02e591
Fergus Ruston <fergus.ruston@kaldorgroup.com>
2023-03-21 13:26:50 +0000

    Session callback set user profilecomplete and username on session

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -52,6 +52,8 @@ export const authOptions: NextAuthOptions = {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
+        session.user.profileComplete = user.profileComplete;
+        session.user.username = user.username;
         // session.user.role = user.role; <-- put other properties on the session here
       }
       return session;
