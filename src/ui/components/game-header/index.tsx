9af686109be37dfeeb5586209f2226293a3ad95b
ergusto <fergus.ruston@kalorgroup.com>
2023-04-23 08:51:50 +0100

    Moved out game detail header into separate component

---
+++ src/ui/components/game-header/index.tsx
@@ -0,0 +1,20 @@
+import type { GameWithUsers } from "@/types";
+import { GameType } from "@prisma/client";
+
+interface GameHeaderProps {
+  game: GameWithUsers;
+  isLoading: boolean;
+}
+
+export default function GameHeader({ game }: GameHeaderProps) {
+  return (
+    <header className="pt-6 pb-10">
+      <h1 className="text-3xl font-bold">{game.title}</h1>
+      <h3 className="text-md mt-4 text-gray-400">
+        {game.gameType === GameType.FIRST_TO
+          ? `First to ${String(game.gameTypeMeta)} points`
+          : `Highest score after ${String(game.gameTypeMeta)} hands`}
+      </h3>
+    </header>
+  );
+}
